2f12cf5c7e09aba87f6cdab3a8006c91a1db7bde
HearthR <anny6159@naver.com>
2019-04-29 21:34:50 +0900

    mysql connect

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var editorRouter = require('./routes/editor');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,11 +27,13 @@ var localTime = function(req, res, next) {
   next();
 }
 
+
 app.use(localTime);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/editor', editorRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/editor.js
+++ routes/editor.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+var db_config = require('../config/db-config.json');
+var conn = mysql.createConnection(db_config);
+
+conn.connect();
+
+var connection_closer = function(next) {
+  conn.end();
+  next();
+};
+
+var test_sql = 'SELECT * FROM stations';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('editor');
+
+  conn.query(test_sql, function(err, rows, fields) {
+    if(err)
+      console.log(err);
+    else
+      //res.send(rows);
+      res.render('editor', {stations:rows});
+  });
+
+  //res.render('editor');
 });
+
+router.use(connection_closer);
 
 module.exports = router;
