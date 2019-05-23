0b78ccece377ee6791c3794e3905b8c5e826d7ad
HearthR <anny6159@naver.com>
2019-05-23 12:58:26 +0900

    db connection 버그 픽스(임시)

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,27 @@
 var express = require('express');
 var router = express.Router();
-var mysql = require('mysql');
+// var mysql = require('mysql');
 var fs = require('fs');
 
-let db_config = require('../config/db-config.json');
-let conn = mysql.createConnection(db_config);
+// let db_config = require('../config/db-config.json');
+// let conn = mysql.createConnection(db_config);
 
-conn.connect();
+// conn.connect();
 
-let connection_closer = function(next) {
-  conn.end();
-  next();
-};
+// let connection_closer = function(next) {
+//   conn.end();
+//   next();
+// };
 
-let list_sql = `SELECT stations.ID, Name, line_num, IFNULL(coord_x, 0) as coord_x, IFNULL(coord_y, 0) as coord_y, coord2_x, coord2_y 
-                FROM stations 
-                LEFT JOIN coordinate 
-                ON stations.ID = coordinate.ID 
-                ORDER BY stations.ID ASC`;
+// let list_sql = `SELECT stations.ID, Name, line_num, IFNULL(coord_x, 0) as coord_x, IFNULL(coord_y, 0) as coord_y, coord2_x, coord2_y 
+//                 FROM stations 
+//                 LEFT JOIN coordinate 
+//                 ON stations.ID = coordinate.ID 
+//                 ORDER BY stations.ID ASC`;
 
-let neighbors_sql = `SELECT ID_from, ID_to, Cost
-                    FROM neighbors
-                    ORDER BY ID_from ASC, ID_TO ASC`;
+// let neighbors_sql = `SELECT ID_from, ID_to, Cost
+//                     FROM neighbors
+//                     ORDER BY ID_from ASC, ID_TO ASC`;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -49,7 +49,7 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.get('/data/stations', function(req, res) {
+router.get('/data/stations', function(req, res, next) {
   fs.readFile('../public/resources/stationData.json', (err, data) => {
     if(err) {
       console.log(err);
@@ -60,7 +60,7 @@ router.get('/data/stations', function(req, res) {
   });
 });
 
-router.get('/data/neighbors', function(req, res) {
+router.get('/data/neighbors', function(req, res, next) {
   fs.readFile('../public/resources/neighborData.json', (err, data) => {
     if(err) {
       console.log(err);
@@ -72,6 +72,6 @@ router.get('/data/neighbors', function(req, res) {
 });
 
 
-router.use(connection_closer);
+// router.use(connection_closer);
 
 module.exports = router;
