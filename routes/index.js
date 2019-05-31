e1ba98262e943969714cac9aecd37d24285fc412
HearthR <anny6159@naver.com>
2019-05-31 23:27:07 +0900

    맵 뷰어 width가 동적으로 변하도록 수정, selectedAll 변수명 변경

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,6 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 
-// var pool = require('../lib/pool/pool.js');
-
-
-// let pair_sql = `SELECT ID_from, ID_to, Total
-//                 FROM congestion
-//                 ORDER BY ID_from ASC, ID_to ASC`;
-
-
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -38,17 +30,6 @@ router.get('/data/neighbors', function(req, res, next) {
   });
 });
 
-// router.get('/pairs', function(req, res, next) {
-//   pool.query(pair_sql, (err, rows) => {
-//     if(err) {
-//       console.log(err);
-//     }
-//     else {
-//       res.send(rows);
-//     }
-//   });
-
-// });
 
 
 module.exports = router;
